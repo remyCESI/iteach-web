22920c026dcda45a4442b1ab0e11617a70e169f7
AlexandreLec <alexandre.lecomte@viacesi.fr>
2019-09-19 21:27:31 +0200

    Add mentions légales

--- JS/contact.js
+++ JS/contact.js
@@ -1,18 +1,12 @@
 $('#sendMail')['click'](function() {
-    email = $('#Email')['val']();
-    message = $('#Message')['val']();
-    sujet = $('#Objet')['val']();
-    if (email['length'] > 0  && message['length'] > 0 && sujet['length'] > 0) {
-        Email['send']({
-            Host: 'smtp.elasticemail.com',
-            Username: 'iteach@outlook.fr',
-            Password: 'ee0474ad-22db-4d7e-91e1-c11c38cb606f',
-            To: 'iteach@outlook.fr',
-            From: 'iteach@outlook.fr',
-            Subject: sujet,
-            Body: 'de:  ' + email + '  son message est:   ' + message
-        })['then'](alert('merci beaucoup'))
-    } else {
-        alert('remplissez tous les champs s\'il vous plait')
-    }
+
+    Email.send({
+        SecureToken : "70021815-0dd3-4edd-912f-15581336147b",
+        To : 'iteach@outlook.fr',
+        From : "iteach@outlook.fr",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      alert("non")
+    );
 })
